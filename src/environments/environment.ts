a3f0b64cafa397f0ca3150e578df6b4785a6435f
Renato Pereira <pereirarenato21@gmail.com>
2021-08-12 10:38:18 -0300

    environment variables updated

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  pdfApiURL: 'http://localhost:3000'
+  production: true,
+  pdfApiURL: 'https://api-mysummary.herokuapp.com/'
 };
 
 /*
