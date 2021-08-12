618525ae8ad063bb6390391288d3bbee5605d426
Renato Pereira <pereirarenato21@gmail.com>
2021-08-12 12:06:34 -0300

    Updating .env port

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.get('/', (request, response) => {
   return response.sendFile(path.join(folderPath, 'index.html'))
 })
 
-const port = process.env.port || 4000
+const port = process.env.PORT || 4000
 app.listen(port, () => {
   console.log(`Server open in port ${port}`)
 })
